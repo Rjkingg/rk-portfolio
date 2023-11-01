14397650718ba4c33a8aac411331874eccd4e82c
mikechavo <mchavez76@yahoo.com>
2023-11-01 15:53:00 -0700

    Cards and social links add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react"
+import './App.css';
 import AboutMe from "./components/AboutMe"
 import ContactInfo from "./components/ContactInfo"
 import Projects from "./components/Projects"
+import { Card, CardBody, CardText, CardTitle, CardSubtitle } from "reactstrap"
 
 const App = () => {
   return (
-    <>
+    <div className="App">
       <h1>Hi, I'm Raquel</h1>
-      <AboutMe/>
-      <ContactInfo/>
-      <Projects/>
-    </>
+      <section>
+        <AboutMe/>
+      </section>
+      <br></br>
+      <section>
+        <ContactInfo/>
+      </section>
+      <br></br>
+      <section>
+        <Projects/>
+      </section>
+    </div>
   )
 }
 
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,12 +1,24 @@
-import React from "react"
-import "../App.css"
+import React from "react";
+import "../App.css";
+import { Card, CardBody, CardText, CardTitle, CardSubtitle } from "reactstrap";
+import github from '../icons/github.png';
+import linkedIn from '../icons/linkedIn.png';
 
 const ContactInfo = () => {
     return(
-        <div>
-            <h3>Contact Info</h3>
-            <p>Details</p>
-        </div>
+        <Card className="card mx-auto"
+        color="primary"
+        inverse
+        style={{
+            width: '18rem'
+          }}>
+            <CardBody>
+            <CardTitle>Contact Info</CardTitle>
+                <a href="https://github.com/Rjkingg"><img src={github} alt="GitHub" className="icon-logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/raquel-king/"><img src={linkedIn} alt="LinkedIn" className="icon-logo" /></a>
+             </CardBody>
+         </Card>
     )
 }
 
